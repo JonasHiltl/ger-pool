576c8e5d64fb31b887be6dbbd885158f080d8033
JonasHiltl <jonashiltl2003@gmail.com>
2021-01-11 21:22:36 +0100

    improving UI

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Flex, Icon, Heading, Text, Center, Spacer, Grid, Box, Link, Circle, Switch, VStack, HStack, Image } from "@chakra-ui/react";
+import { Wrap, WrapItem, Flex, Icon, Heading, Text, Center, Spacer, Grid, Box, Link, Circle, Switch, VStack, HStack, Image, Accordion, AccordionButton, AccordionItem, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
 import { useRouter } from 'next/router';
 import { en, de } from '../translations'
 
@@ -32,7 +32,7 @@ export default function Home() {
     <Box pos='relative' h='100vh'>
       <Flex px={[ 5, 6, 16, 32 ]} py={[ 6, 6 ]} alignItems='center'>
         {/* <Image src='Logo.svg'w='60px' mr={[ 2, 4, 6 ]} /> */}
-        <Heading color='gray.900'>GER Pool</Heading>
+        <Heading color='gray.900' size="2xl" >GER Pool</Heading>
         <Spacer />
         <HStack spacing='4px'>
           <Image 
@@ -63,7 +63,7 @@ export default function Home() {
         <Icon as={FiGithub} color='gray.700' w={8} h={8}/>
         </Link>
       </Grid>
-      <Box px={[ 5, 6, 16, 32 ]} minHeight='90vh'>
+      <Box px={[ 5, 6, 16, 32 ]} minHeight='50vh'>
         <Box mt='auto'>
           <Heading mb='10px'>{l.h1Network}</Heading>
           <Flex>
@@ -88,16 +88,70 @@ export default function Home() {
           </Flex>
         </Box>
       </Box>
+      <Wrap px={[ 5, 6, 32, '23%' ]} justifyContent='space-between'>
+        <WrapItem w={[ '100%', '100%', '30%']}>
+          <Box textAlign='center'>
+            <Image w={[ 36, 36, 36 ]} mx='auto' my={3} src='decentralized.svg' alignItems='center'/>
+            <Heading size="xl" my={3}>Test</Heading>
+            <Text my={3} fontSize="sm" opacity='0.6'>Operating an environment-friendly 5 Watt stakepool 24/7 with 99.9% uptime</Text>
+          </Box>
+        </WrapItem>
+        <WrapItem w={[ '100%', '100%', '30%']}>
+          <Box textAlign='center'>
+            <Image w={[ 36, 36, 36 ]} mx='auto' my={3} src='support.svg' alignItems='center'/>
+            <Heading size="xl" my={3}>24/7 Support</Heading>
+            <Text my={3} fontSize="sm" opacity='0.6'>Highly reliable because of very good Internet connection in order to get all my slots</Text>
+          </Box>
+        </WrapItem>
+        <WrapItem w={[ '100%', '100%', '30%']}>
+          <Box textAlign='center'>
+            <Heading size="xl">Test</Heading>
+            <Text fontSize="sm" opacity='0.6'>Running my stakepool from home, helping to truly decentralize the Cardano ecosystem</Text>
+          </Box>
+        </WrapItem>
+        <WrapItem>
+        </WrapItem>
+      </Wrap>
+      <Accordion allowToggle px={[ 5, 6, 32, '23%' ]}>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 1 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 2 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
       <Box bgImage="url('/bg.svg')" h={[ '500px' ,'704px']} bgPosition='center' bgRepeat='no-repeat' bgSize='cover' position='relative'>
         <Box position='absolute' top='50%' left='50%' transform='translate(-50%, -50%)' w='100%' p={[ 5, 6, 16, 32 ]}>
           <Box h='1px' backgroundColor='gray.50' opacity='0.2' mb='20px'/>
           <Flex justifyContent='space-between'>
             <Box>
-              <Text fontSize="sm" color='gray.50' opacity='0.5'>Have any question?</Text>
+              <Text fontSize="sm" color='gray.50' opacity='0.5'>{l.pAnyQuestios}</Text>
               <Text fontSize="sm" color='gray.50'>jonashiltl@gmx.net</Text>
             </Box>
             <Flex direction={['column', 'column', 'column', 'column']}>
-              <Text fontSize="sm" color='gray.50'  opacity='0.5' mr={[0, 0, 1]}>Get more information:</Text>
+              <Text fontSize="sm" color='gray.50'  opacity='0.5' mr={[0, 0, 1]}>{l.pMoreInformation}</Text>
               <Flex>
                 <Link ml='auto' mr='0px'>
                   <Icon as={FiTwitter} color='gray.50' w={4}/>
--- translations.js
+++ translations.js
@@ -1,11 +1,15 @@
 export const en = {
     h1Network: "Homegrown network",
     pText1: "Many Stake Pools are hosted on AWS, Microsoft or Google cloud infrastructures and therefor controlled by these entities.",
-    pText2: "We aim to keep decentralizing the Cardano ecosystem by running our node on a powerful Raspberry Pi network."
+    pText2: "We aim to keep decentralizing the Cardano ecosystem by running our node on a powerful Raspberry Pi network.",
+    pMoreInformation: "Get more information:",
+    pAnyQuestios: "Have any question?",
 }
 
 export const de = {
     h1Network: "Heimisches Netzwerk",
     pText1: "Viele Stake Pools werden auf AWS, Microsoft oder Google Cloud-Infrastrukturen gehostet und daher von diesen Unternehmen kontrolliert.",
-    pText2: "Unser Ziel ist es, dass Cardano-Ökosystem weiter zu dezentralisieren, indem wir unseren Pool auf einem leistungsstarken Raspberry Pi-Netzwerk betreiben."
+    pText2: "Unser Ziel ist es, dass Cardano-Ökosystem weiter zu dezentralisieren, indem wir unseren Pool auf einem leistungsstarken Raspberry Pi-Netzwerk betreiben.",
+    pMoreInformation: "Erfahren Sie mehr:",
+    pAnyQuestios: "Haben Sie Fragen?",
 }
