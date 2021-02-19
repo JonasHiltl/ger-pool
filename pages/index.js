ae8a1a49423a898e4cecf7a15f12c476d4db15d4
JonasHiltl <jonashiltl2003@gmail.com>
2021-02-19 23:48:01 +0100

    height

--- pages/index.js
+++ pages/index.js
@@ -248,10 +248,10 @@ export default function Home() {
               <Text mb="8px">{l.usEducationText1}</Text>
               <Text>{l.usEducationText2}</Text>
               <Wrap w="100%" my="30px" justify="space-evenly">
-                <Image src="screen1.PNG" maxWidth={["120px", "150px", "180px"]}/>
-                <Image src="screen2.PNG" maxWidth={["120px", "150px", "180px"]}/>
-                <Image src="screen3.PNG" maxWidth={["120px", "150px", "180px"]}/>
-                <Image src="screen4.PNG" maxWidth={["120px", "150px", "180px"]}/>
+                <Image src="screen1.PNG" maxWidth={["120px", "150px", "180px"]} h="auto"/>
+                <Image src="screen2.PNG" maxWidth={["120px", "150px", "180px"]} h="auto"/>
+                <Image src="screen3.PNG" maxWidth={["120px", "150px", "180px"]} h="auto"/>
+                <Image src="screen4.PNG" maxWidth={["120px", "150px", "180px"]} h="auto"/>
               </Wrap>
               <Flex align='center' mb='8px'>
                 <Icon color='#01DE86' as={BsBook} color='gray.700' w={8} h={8} mr='8px'/>
