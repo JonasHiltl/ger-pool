cc146b7f1b664f48fdbc8d4bc9ac6599c1afc305
JonasHiltl <jonashiltl2003@gmail.com>
2021-02-19 23:33:21 +0100

    image size

--- pages/index.js
+++ pages/index.js
@@ -248,10 +248,10 @@ export default function Home() {
               <Text mb="8px">{l.usEducationText1}</Text>
               <Text>{l.usEducationText2}</Text>
               <Wrap w="100%" my="30px" justify={[ "space-evenly", "space-evenly"]} shouldWrapChildren="true" spacing="20px">
-                <Image src="screen1.PNG" width={["120px", "150px", "180px"]} height="auto" mx="10px"/>
-                <Image src="screen2.PNG" width={["120px", "150px", "180px"]} height="auto" mx="10px"/>
-                <Image src="screen3.PNG" width={["120px", "150px", "180px"]} height="auto" mx="10px"/>
-                <Image src="screen4.PNG" width={["120px", "150px", "180px"]} height="auto" mx="10px"/>
+                <Image src="screen1.PNG" width={["120px", "150px", "180px"]} height={["90", "112", "135"]} mx="10px"/>
+                <Image src="screen2.PNG" width={["120px", "150px", "180px"]} height={["90", "112", "135"]} mx="10px"/>
+                <Image src="screen3.PNG" width={["120px", "150px", "180px"]} height={["90", "112", "135"]} mx="10px"/>
+                <Image src="screen4.PNG" width={["120px", "150px", "180px"]} height={["90", "112", "135"]} mx="10px"/>
               </Wrap>
               <Flex align='center' mb='8px'>
                 <Icon color='#01DE86' as={BsBook} color='gray.700' w={8} h={8} mr='8px'/>
